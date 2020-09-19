96d641d6955886325c972263e89707ae122f0025
Andres Moreno <andresmore06@gmail.com>
2020-09-19 09:48:00 -0500

    Dojo finished

---
+++ src/components/ImageList.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col v-for="item in items" v-bind:key="item.id" sm="4" cols="12">
+        <v-card>
+          <img :src="item.previewURL"/>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "ImageList",
+  props: ["items"]
+};
+</script>
+
+<style>
+</style>
---
+++ src/components/ToolBar.vue
@@ -0,0 +1,23 @@
+<template>
+   <div>
+    <v-app-bar
+      color="indigo accent-4"
+      dense
+      dark
+    >
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Galeria</v-toolbar-title>
+    </v-app-bar>
+  </div>
+</template>
+
+<script>
+export default {
+  name:'ToolBar'
+}
+</script>
+
+<style>
+
+</style>
